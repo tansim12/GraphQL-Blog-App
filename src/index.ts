4b177c83eb97f9906bc440aac2efe11ceae15f22
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-01-20 21:47:13 +0600

    refactor schema and type dfns

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,9 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+import {typeDefs} from './schema';
+import {resolvers} from './resolvers';
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
 
 // Passing an ApolloServer instance to the `startStandaloneServer` function:
 //  1. creates an Express app
